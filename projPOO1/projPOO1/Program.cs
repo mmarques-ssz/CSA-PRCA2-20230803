bd80d950badd0672d2e28e47b2fec1e696a41786
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-08-03 21:11:39 -0300

    Add files via upload

---
+++ projPOO1/projPOO1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma s;
+            s = new Soma();
+
+            s.n1 = 5;
+            s.n2 = 6;
+            s.calcular();
+            Console.WriteLine("A soma de {0} com {1} é {2}", 
+                s.n1, s.n2, s.resultado);
+        }
+    }
+}
---
+++ projPOO1/projPOO1/Soma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO1
+{
+    internal class Soma
+    {
+        // Atributos
+        public int n1;
+        public int n2;
+        public int resultado;
+
+        // Métodos
+        public void calcular()
+        {
+            resultado = n1 + n2;
+        }
+    }
+}
